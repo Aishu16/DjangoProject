a9d0fbcb7a8dca74621fcc6b0e56372e7e406dce
aishwarya <aishwaryawagh94@gmail.com>
2020-09-25 19:28:25 -0400

    Created URLS file for firstapp

---
+++ firstproject/firstapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from firstapp import views
+
+urlpatterns = [
+    path('home', views.home),
+    path('about', views.about),
+    path('contact-us', views.contact),
+
+]
